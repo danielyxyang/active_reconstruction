8815b9751416e8cee71f05c63a969150e34dd5aa
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2023-04-16 21:19:59 +0200

    NEW - added ability for hierarchical CheckboxList and colored label text

--- src/utils/widgets.py
+++ src/utils/widgets.py
@@ -184,24 +184,53 @@ class CheckboxList(widgets.VBox, widgets.widget_description.DescriptionWidget, w
     value = Any(help="Selected checkboxes")
 
     def __init__(self, options, value=[], colors={}, description=None, **kwargs):
-        self.options = options
-        self.checkboxes = {}
-        # create description label
-        label = [widgets.Label(value=description)] if description is not None else []
-        # create checkboxes
-        for name in options:
-            # value = value[name] if name in value.keys() else True
-            self.checkboxes[name] = widgets.Checkbox(value=name in value, indent=False, layout=widgets.Layout(width="auto", margin="0 0 0 20px"), description=name) 
-            self.checkboxes[name].style.background = colors.get(name, "")
-        checkbox_list = [widgets.VBox([self.checkboxes[name] for name in options], **kwargs)]
-        # register handler to update widget value
-        for checkbox in self.checkboxes.values():
+        # create checkbox widgets
+        checkboxes = {}
+        checkboxes_list = []
+        for option in options:
+            option_path = option.split(".")
+            parent_all = ".".join(option_path[:-1] + ["*"])
+            # create checkbox
+            checkbox = widgets.Checkbox(
+                value=option in value or parent_all in value,
+                indent=False,
+                layout=dict(width="auto", margin="0px"),
+            )
+            checkbox_label = widgets.Label(
+                value=option_path[-1],
+                layout=dict(margin="0px"),
+                style=dict(text_color=colors.get(option, colors.get(parent_all, None))),
+            )
+            checkbox_widget = widgets.HBox(
+                [checkbox, checkbox_label],
+                layout=dict(overflow="visible", padding="0 10px 0 10px", margin="0 0 0 {}px".format(len(option_path[:-1])*30)),
+            )
+            # register handler to update widget value
             checkbox.observe(self.__update_value, names="value")
 
-        super().__init__(children=label + checkbox_list)
+            checkboxes[option] = checkbox
+            checkboxes_list.append(checkbox_widget)
+
+        # create label and container widgets
+        children = []
+        if description is not None:
+            children.append(widgets.Label(value=description))
+        children.append(widgets.VBox(checkboxes_list, **kwargs))
+        super().__init__(children=children)
+
+        self.checkboxes = checkboxes
         self.__update_value()
     
     # PRIVATE METHODS
-    
+
     def __update_value(self, *args):
-        self.value = {name: self.checkboxes[name].value for name in self.options}
+        value = {}
+        for option, checkbox in self.checkboxes.items():
+            option_path = option.split(".")
+            if len(option_path) == 1:
+                value[option] = checkbox.value
+            else:
+                parent = ".".join(option_path[:-1])
+                value[option] = value[parent] and checkbox.value
+                checkbox.disabled = not value[parent]
+        self.value = value
